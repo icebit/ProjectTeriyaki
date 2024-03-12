0c585763757591219783a49ba9075df62c66dc65
icebit <epicpotatopro@gmail.com>
2024-03-11 20:25:40 -0500

    Added player and fast food restaurant

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Sup");
     }
 
     // Update is called once per frame
